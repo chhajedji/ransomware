eeea67354b8f6cc14faec115a6dc9e90e65838a5
Chinmay Chhajed <chhajedchinmay@gmail.com>
2022-06-13 22:06:26 -0400

    Iterate recursively in given directory to encrypt and decrypt all the files.

--- decrypt.py
+++ decrypt.py
@@ -3,28 +3,38 @@
 import os, sys
 from cryptography.fernet import Fernet
 
-# Set this as the root directory from where you need to encrypt all the files.
-root_dir = "test_dir"
+
+# If not input is given, then set current directory as the root
+# directory from where you need to encrypt all the files.
+
+if len(sys.argv) > 1:
+    root_dir = sys.argv[1]
+else:
+    root_dir = 'test_dir'
 
 all_files = []
 
-#  for file in os.listdir():
-for file in os.listdir(root_dir):
+def list_files(base_dir):
+    global all_files
+    for entry in os.listdir(base_dir):
 
-    # sys.argv[0] is the name of current file.
-    # Be sure to not encrypt important files like the key or the
-    # encryption and decryption scripts. As long as the `root_dir' is
-    # different from the current directory, this should not be an issue.
-    if file == sys.argv[0] or file == "key.key" or file == "encrypt.py":
-        continue
+        # sys.argv[0] is the name of current file.
+        # Be sure to not encrypt important files like the key or the
+        # encryption and decryption scripts. As long as the `root_dir' is
+        # different from the current directory, this should not be an issue.
+        if entry == sys.argv[0] or entry == "key.key" or entry == "decrypt.py":
+            continue
 
-    # Since test directory is different from the current file
-    # location, we need to give the absolute path for the file to
-    # check if it's a file or a directory.
-    if os.path.isfile(os.path.join(root_dir, file)):
-        all_files.append(os.path.join(root_dir, file))
+        if os.path.isdir(os.path.join(base_dir, entry)):
+            list_files(os.path.join(base_dir, entry))
 
-# print("Files: ", all_files)
+        elif os.path.isfile(os.path.join(base_dir, entry)):
+            all_files.append(os.path.join(base_dir, entry))
+        # print("****\nentry: {}\nall_files: {}\n****\n".format(entry, all_files))
+
+key = Fernet.generate_key()
+
+list_files(root_dir)
 
 with open("key.key", "rb") as thekey:
     code = thekey.read()
